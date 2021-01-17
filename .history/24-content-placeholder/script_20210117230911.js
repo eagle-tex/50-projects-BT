6f9414fde92fb1df75a7eaeb747e8933365b25d1
Gildas Montcho <gmontcho@gmail.com>
2021-01-17 23:17:32 +0100

    project 24-content-placeholder done

---
+++ .history/24-content-placeholder/script_20210117230911.js
@@ -0,0 +1,15 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="">';
+  title.innerHTML = 'Lorem ipsum dolor sit amet.';
+}
