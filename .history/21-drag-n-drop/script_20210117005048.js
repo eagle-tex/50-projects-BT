7573ba2965cc214afbdf053d5f8a1b95e6de5bb2
Gildas Montcho <gmontcho@gmail.com>
2021-01-17 01:03:51 +0100

    project 21-drag-n-drop done

---
+++ .history/21-drag-n-drop/script_20210117005048.js
@@ -0,0 +1,24 @@
+//
+function dragStart() {
+  console.log('drag start')
+}
+
+function dragEnd() {
+  console.log('drag start')
+}
+
+function dragStart() {
+  console.log('drag start')
+}
+
+function dragStart() {
+  console.log('drag start')
+}
+
+function dragStart() {
+  console.log('drag start')
+}
+
+function dragStart() {
+  console.log('drag start')
+}
---
+++ .history/21-drag-n-drop/script_20210117005109.js
@@ -0,0 +1,24 @@
+//
+function dragStart() {
+  console.log('drag start');
+}
+
+function dragEnd() {
+  console.log('drag start');
+}
+
+function dragOver() {
+  console.log('drag start');
+}
+
+function dragEnter() {
+  console.log('drag start');
+}
+
+function dragLeave() {
+  console.log('drag start');
+}
+
+function dragDrop() {
+  console.log('drag start');
+}
