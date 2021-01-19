b901e0f7eadfc8d471baa320ff6fa1fcf07e46e8
Gildas Montcho <gmontcho@gmail.com>
2021-01-19 22:03:19 +0100

    project 26-double-vertical-slider done

---
+++ .history/26-double-vertical-slider/script_20210119070343.js
@@ -0,0 +1,7 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideRight = document.querySelector('.right-slide');
+const sliderContainer = document.querySelector('.slider-container');
+const sliderContainer = document.querySelector('.slider-container');
+const sliderContainer = document.querySelector('.slider-container');
+const sliderContainer = document.querySelector('.slider-container');
---
+++ .history/26-double-vertical-slider/script_20210119070417.js
@@ -0,0 +1,7 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const sliderContainer = document.querySelector('.up-button');
+const sliderContainer = document.querySelector('.up-button');
+const sliderContainer = document.querySelector('.slider-container');
+const sliderContainer = document.querySelector('.slider-container');
