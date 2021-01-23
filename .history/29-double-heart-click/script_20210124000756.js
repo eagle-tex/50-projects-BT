2f9c556235015d1eaa4a8cb6df0a36ba4a5211ff
Gildas Montcho <gmontcho@gmail.com>
2021-01-24 00:26:02 +0100

    project 29-double-heart-click done

---
+++ .history/29-double-heart-click/script_20210124000756.js
@@ -0,0 +1,4 @@
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+loveMe.addEventListener('dblclick');
---
+++ .history/29-double-heart-click/script_20210124001516.js
@@ -0,0 +1,21 @@
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let clickTime = 0;
+
+const createHeart = (e) => {
+  const heart = document.createElement('i');
+  heart.classList.add('fas fa-heart');
+  // heart.classList.add('fas');
+};
+
+loveMe.addEventListener('click', (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+  } else if (new Date().getTime() - clickTime < 800) {
+    createHeart(e);
+    clickTime = 0;
+  } else {
+    clickTime = new Date().getTime();
+  }
+});
